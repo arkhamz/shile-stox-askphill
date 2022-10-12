703ca4a0bd191746b71090c4e189d0bfe6863f93
arkhamz <shile2007@gmail.com>
2022-10-12 09:58:44 +0200

    sliding swiper completed and styled

--- src/components/CategorySlider.js
+++ src/components/CategorySlider.js
@@ -1,16 +1,35 @@
 import "./CategorySlider.css"
 import { slides } from "../utils"
+import {Swiper, SwiperSlide} from "swiper/react"
+import "swiper/css";
 
 export default function CategorySlider(){
 
-    
     return (
-        <div className="slider">
+        <Swiper
+            // spaceBetween={0}
+            slidesPerView={2}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            {slides.map(function(item,index){
+                return <SwiperSlide key={index}>
+                   <div  className="slidepic-container">
+                    <img className="slidepic" src={item} alt="load error" />
+                </div>
+                    </SwiperSlide>
+            })}
+
+        </Swiper>
+    )
+}
+
+
+
+{/* <div className="slider">
             {slides.map(function(item,index){
                 return <div key={index}  className="slidepic-container">
                     <img className="slidepic" src={item} alt="load error" />
                 </div>
             })}
-        </div>
-    )
-}
+        </div> */}
