ecf4b450ea881d79c2bae28478af1774f15937a4
arkhamz <shile2007@gmail.com>
2022-10-12 12:55:20 +0200

    added fade animation to slider

--- src/components/CategorySlider.js
+++ src/components/CategorySlider.js
@@ -2,10 +2,11 @@ import "./CategorySlider.css"
 import { slides } from "../utils"
 import {Swiper, SwiperSlide} from "swiper/react"
 import "swiper/css";
-
+import SpringFade from "./Fade";
 
 
 export default function CategorySlider(){
+    // testing
 
     return (
         <Swiper
@@ -18,18 +19,23 @@ export default function CategorySlider(){
                 if(index === 2){
                     return <SwiperSlide  key={item.id}>
                  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-                 <div  className="slidepic-container">
-                    <img className="slidepic" src={item.src} alt="load error" />
-                    <h2 className="slide-caption">{item.caption}</h2>
-                </div>
+                    <SpringFade>
+                        <div  className="slidepic-container">
+                            <img className="slidepic" src={item.src} alt="load error" />
+                            <h2 className="slide-caption">{item.caption}</h2>
+                        </div>
+                    </SpringFade>
+                 
                  </a>
                     </SwiperSlide> 
                 } else {
                     return <SwiperSlide  key={item.id}>
+                   <SpringFade>
                    <div  className="slidepic-container">
                     <img className="slidepic" src={item.src} alt="load error" />
                     <h2 className="slide-caption">{item.caption}</h2>
                 </div>
+                   </SpringFade>
                     </SwiperSlide>
                 }
             })}
